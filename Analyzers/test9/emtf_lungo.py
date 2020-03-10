fc89e0ae9633569e253c12fc8e46ac83d6dbacc8
Jia Fu Low <jia.fu.low@cern.ch>
2020-03-10 17:03:25 -0400

    Update test9 [34]

--- Analyzers/test9/emtf_lungo.py
+++ Analyzers/test9/emtf_lungo.py
@@ -66,7 +66,7 @@ class ZoneAnalysis(_BaseAnalysis):
   Description.
   """
 
-  def run(self, algo, pileup=200):
+  def run(self, algo):
     # Overwrite eta bins
     eta_bins = (0.8, 1.2, 1.55, 1.98, 2.5)
     eta_bins = eta_bins[::-1]
@@ -123,6 +123,100 @@ class ZoneAnalysis(_BaseAnalysis):
           print(zone, '%03i' % lay, '%5i' % n, p, q)
     return
 
+# ______________________________________________________________________________
+class UnittestAnalysis(_BaseAnalysis):
+  """Unit testing.
+
+  Description.
+  """
+
+  def run(self, algo):
+    out_part = []
+    out_hits = []
+    out_tracks = []
+
+    # __________________________________________________________________________
+    # Load tree
+    def load_pgun_test():
+      infile = '../test7/ntuple.root'
+      return load_tree(infile)
+
+    tree = load_pgun_test()
+    verbosity = 1
+
+    # Loop over events
+    for ievt, evt in enumerate(tree):
+      if maxevents != -1 and ievt == maxevents:
+        break
+
+      # Particles
+      ievt_part = None
+      for ipart, part in enumerate(evt.particles):
+        part_id = (part.pdgid,)
+        part_val = (part.pt, part.eta, part.phi, part.invpt, part.d0)
+        ievt_part = (part_id + part_val)
+
+      # Trigger primitives
+      ievt_hits = []
+      for ihit, hit in enumerate(evt.hits):
+        hit_id = (hit.type, hit.station, hit.ring, find_endsec(hit.endcap, hit.sector), hit.bx)
+        hit_val = (hit.emtf_phi, hit.emtf_theta, hit.bend, hit.quality, hit.time, hit.strip, hit.wire, hit.roll, hit.fr, hit.sim_tp1, hit.sim_tp2)
+        ievt_hits.append(hit_id + hit_val)
+
+      # Tracks
+      ievt_tracks = []
+      for itrk, trk in enumerate(evt.tracks):
+        trk_id = (find_endsec(trk.endcap, trk.sector), trk.mode)
+        trk_val = (trk.pt, trk.xml_pt, trk.phi, trk.theta, trk.eta, trk.q, trk.bx, trk.nhits, trk.hitref1, trk.hitref2, trk.hitref3, trk.hitref4)
+        ievt_tracks.append(trk_id + trk_val)
+
+      # Finally, add to output
+      out_part.append(ievt_part)
+      out_hits.append(ievt_hits)
+      out_tracks.append(ievt_tracks)
+
+    # End loop over events
+
+    # __________________________________________________________________________
+    # Output
+    outfile = 'unittest.npz'
+    if use_condor:
+      outfile = outfile[:-4] + ('_%i.npz' % jobid)
+    out_part = np.asarray(out_part)
+    out_hits = create_ragged_array(out_hits)
+    out_tracks = create_ragged_array(out_tracks)
+    print('[INFO] out_part: {0} out_hits: {1} out_tracks: {2}'.format(out_part.shape, out_hits.shape, out_tracks.shape))
+    out_dict = {
+      'out_part': out_part,
+      'out_hits_values': out_hits.values,
+      'out_hits_row_splits': out_hits.row_splits,
+      'out_tracks_values': out_tracks.values,
+      'out_tracks_row_splits': out_tracks.row_splits,
+    }
+    save_np_arrays(outfile, out_dict)
+
+    # __________________________________________________________________________
+    # Assert
+    def load_unittest(f):
+      with np.load(f) as loaded:
+        out_part = loaded['out_part']
+        out_hits = RaggedTensorValue(loaded['out_hits_values'], loaded['out_hits_row_splits'])
+        out_tracks = RaggedTensorValue(loaded['out_tracks_values'], loaded['out_tracks_row_splits'])
+      return (out_part, out_hits, out_tracks)
+
+    reference = '../test7/unittest_RelVal111X.npz'
+    out_part_ref, out_hits_ref, out_tracks_ref = load_unittest(reference)
+    print('[INFO] out_part_ref: {0} out_hits_ref: {1} out_tracks_ref: {2}'.format(out_part_ref.shape, out_hits_ref.shape, out_tracks_ref.shape))
+
+    import numpy.testing as npt
+    npt.assert_allclose(out_part, out_part_ref)
+    print('[INFO] Test 1 OK')
+    npt.assert_allclose(out_hits.values, out_hits.values)
+    print('[INFO] Test 2 OK')
+    npt.assert_allclose(out_tracks.values, out_tracks.values)
+    print('[INFO] Test 3 OK')
+    return
+
 
 # ______________________________________________________________________________
 # Main
@@ -135,7 +229,8 @@ algo = 'default'  # phase 2
 
 # Analysis mode (pick one)
 #analysis = 'dummy'
-analysis = 'zone'
+#analysis = 'zone'
+analysis = 'unittest'
 
 # Job id (pick an integer)
 jobid = 0
@@ -170,6 +265,10 @@ def main():
 
   elif analysis == 'zone':
     anna = ZoneAnalysis()
+    anna.run(algo=algo)
+
+  elif analysis == 'unittest':
+    anna = UnittestAnalysis()
     anna.run(algo=algo)
 
   else:
